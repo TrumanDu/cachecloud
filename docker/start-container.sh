80e51291eaeda078534d68ea082cabe68f5f30fd
truman.p.du <1753177225@qq.com>
2017-03-21 16:30:38 +0800

    update
    update

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -10,13 +10,20 @@ SERVER_NAME=cachecloud
 DEPLOY_DIR=/opt/cachecloud-web
 STDOUT_FILE=${DEPLOY_DIR}/logs/cachecloud-web.log
 WAR_FILE=${DEPLOY_DIR}/cachecloud-open-web-1.0-SNAPSHOT.war
-
-
-JAVA_OPTS="-server -Xmx4g -Xms4g -Xss256k -XX:MaxDirectMemorySize=1G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=40 -XX:+PrintGCDateStamps -Xloggc:/opt/cachecloud-web/logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/cachecloud-web/logs/java.hprof -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+PrintCommandLineFlags -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.util.Arrays.useLegacyMergeSort=true -Dfile.encoding=UTF-8"
+cd /tmp/cachecloud-${CACHECLOUD_VERSION}/cachecloud-open-web/target/cachecloud-open-web-1.0-SNAPSHOT/WEB-INF/classes
+if [[ -n "$cachecloud-db-url" ]]; then
+sed -i "s/^.*cachecloud.db.url.*$/cachecloud.db.url = $cachecloud-db-url/" application.properties
+fi
+if [[ -n "$cachecloud-db-user" ]]; then
+sed -i "s/^.*cachecloud.db.user.*$/cachecloud.db.user = $cachecloud-db-user/" application.properties
+fi
+if [[ -n "$cachecloud-db-password" ]]; then
+sed -i "s/^.*cachecloud.db.password.*$/cachecloud.db.password = $cachecloud-db-password/" application.properties
+fi
+cd /tmp/cachecloud-${CACHECLOUD_VERSION}/cachecloud-open-web/target/
+jar cf cachecloud-open-web-1.0-SNAPSHOT.war -C cachecloud-open-web-1.0-SNAPSHOT/ .
+cp /tmp/cachecloud-${CACHECLOUD_VERSION}/cachecloud-open-web/target/cachecloud-open-web-1.0-SNAPSHOT.war ${base_dir}
+JAVA_OPTS="-server -Xmx4g -Xms1g -Xss256k -XX:MaxDirectMemorySize=1G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:G1ReservePercent=25 -XX:InitiatingHeapOccupancyPercent=40 -XX:+PrintGCDateStamps -Xloggc:/opt/cachecloud-web/logs/gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/cachecloud-web/logs/java.hprof -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+PrintCommandLineFlags -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Djava.util.Arrays.useLegacyMergeSort=true -Dfile.encoding=UTF-8"
 echo -e "Starting the ${SERVER_NAME} ...\c"
 
-if [[ -n "$config" ]]; then
-  java $JAVA_OPTS -jar ${WAR_FILE} --spring.application.json=\'$config\' > $STDOUT_FILE 2>&1
-else
-  java $JAVA_OPTS -jar ${WAR_FILE} > $STDOUT_FILE 2>&1
-fi
+ java $JAVA_OPTS -jar ${WAR_FILE}  > $STDOUT_FILE 2>&1
