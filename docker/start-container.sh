f2fe8cf40ff151aef59814613bea69f5da9657cf
truman.p.du <1753177225@qq.com>
2017-03-22 13:55:14 +0800

    update
    update

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -15,6 +15,7 @@ cd /tmp/cachecloud-${CACHECLOUD_VERSION}/cachecloud-open-web/target/
 if [ ! -d 'newtmp' ]; then
  mkdir newtmp
 unzip cachecloud-open-web-1.0-SNAPSHOT.war -d newtmp
+rm -f cachecloud-open-web-1.0-SNAPSHOT.war
 cd /tmp/cachecloud-${CACHECLOUD_VERSION}/cachecloud-open-web/target/cachecloud-open-web-1.0-SNAPSHOT/WEB-INF/classes
 if [[ -n "$cachecloud-db-url" ]]; then
 sed -i "s/^.*cachecloud.db.url.*$/cachecloud.db.url = $cachecloud_db_url/" application.properties
